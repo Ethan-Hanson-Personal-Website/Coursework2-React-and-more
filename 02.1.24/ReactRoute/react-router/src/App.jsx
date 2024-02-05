a43ea79ac511c75669489607fa70ce12f561373d
Alivar <ethanhanson9@gmail.com>
2024-02-04 18:07:49 -0800

    Route React HW

--- 02.1.24/ReactRoute/react-router/src/App.jsx
+++ 02.1.24/ReactRoute/react-router/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+import pages from './components/pages'
+import Home from './components/home'
 import './App.css'
 
 function App() {
@@ -9,11 +11,16 @@ function App() {
     <>
       <div id="container">
         <h1>Hello React Router!</h1>
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Link to="/blue">Blue</Link>
+          <Link to="/">Home</Link>
+          <Link to="/red">Red</Link>
+        </div>
         <div id="main-section">
           <Routes>
-            <Route path="/blue" element={<h1>Blue</h1>} />
-            <Route path="/red" element={<h1>Red</h1>} />
+            <Route path="/blue" element={<pages.Blue />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/red" element={<pages.Red />} />
           </Routes>
         </div>
       </div>
